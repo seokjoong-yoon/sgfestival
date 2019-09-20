94e317a0ae4e6dff09bf9cac66b23fbd0cfac89d
seokjoong-yoon <dyd3671@naver.com>
2019-09-20 19:12:22 +0900

    schedule pagination backend

--- schedule/models.py
+++ schedule/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+EVENTDAY_CHOICE = (
+    ('mon', 'mon'),
+    ('tue', 'tue'),
+    ('wed', 'wed'),
+    ('thu', 'thu'),
+    ('fri', 'fri'),
+)
+
 class Schedule(models.Model):
+    eventDay = models.CharField(max_length=100, choices = EVENTDAY_CHOICE, default='mon')
     eventImage = models.ImageField(upload_to='images/', blank=True)
     eventContent = models.CharField(max_length = 300)
     eventTitle = models.CharField(max_length = 200)
+    

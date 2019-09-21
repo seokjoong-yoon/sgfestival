d4856fa64a68867e87b4d35bada86ff5a5190765
student513 <compa513@naver.com>
2019-09-21 16:34:10 +0900

    schedule complete

--- sgfestival/urls.py
+++ sgfestival/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static  
 import madang.views as mviews
 
 
@@ -29,4 +31,4 @@ urlpatterns = [
     path('foodtruck/', include("foodtruck.urls")),
     path('timetable/',mviews.timetable,name="timetable"),
 ]
-
+urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
